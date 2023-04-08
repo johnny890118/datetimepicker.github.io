8b16e7399c9d5b63da376f46e46448688b5e78ba
johnny890118 <128392036+johnny890118@users.noreply.github.com>
2023-04-09 05:32:08 +0800

    datetimepicker

---
+++ datetimepicker/script.js
@@ -0,0 +1,10 @@
+jQuery.datetimepicker.setLocale("zh-TW");
+
+jQuery("#checkIn").datetimepicker({
+  timepicker: false,
+  format: "Y/m/d",
+});
+jQuery("#checkOut").datetimepicker({
+  timepicker: false,
+  format: "Y/m/d",
+});
